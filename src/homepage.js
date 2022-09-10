6698dc236ad61569b2cef356b59557cb7b02f363
Max N <smth@gmail.com>
2022-09-11 06:13:10 +0700

    flags added

--- src/homepage.js
+++ src/homepage.js
@@ -7,9 +7,11 @@ const homepage = posts => `
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="${config.blogDescription}" />
-        <link rel="stylesheet" href="./assets/styles/grotesk.light.min.css">
-        <link rel="stylesheet" href="./assets/styles/main.min.css">
+        <link rel="stylesheet" href = "../../src/assets/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+        
         <title>${config.blogName}</title>
     </head>
     <body>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
-const fs = require("fs");
-const path = require('path')
+#!/usr/bin/env node
 
-const postMethods = require("./posts");
-const config = require("./config");
-const addHomePage = require("./homepage");
+// console.log(process.argv);
 
-const posts = fs
-  .readdirSync(config.dev.postsdir)
-  .map(post => post.slice(0, -4))
-  .map(post => postMethods.createPost(post))
-  .sort(function(a, b) {
-    return b.attributes.date - a.attributes.date;
-  });
+const args = process.argv;
 
+args.forEach(arg => {
+  if(arg === '--help' || arg === '-h') {
+    console.log('\x1b[33m%s\x1b[0m' ,'Write simple-ssg1 and add flag -i with txt file as an argument\nWrite simple-ssg1 -v to see the name and version of the tool');
+  }
 
-if (!fs.existsSync(config.dev.outdir)) fs.mkdirSync(config.dev.outdir);
+  else if(arg === '--version' || arg === '-v') {
+    console.log('\x1b[36m%s\x1b[0m', 'simple-ssg1\nVersion 1.0');
+  }
 
-postMethods.createPosts(posts);
-addHomePage(posts);
+  else if(arg === '--input' || arg === '-i'){
+    const fs = require("fs");
+    const path = require('path')
+    
+    const postMethods = require("./posts");
+    const config = require("./config");
+    const addHomePage = require("./homepage");
+    
+    const posts = fs
+      .readdirSync(config.dev.postsdir)
+      .map(post => post.slice(0, -4))
+      .map(post => postMethods.createPost(post))
+      .sort(function(a, b) {
+        return b.attributes.date - a.attributes.date;
+      });
+    
+    
+    if (!fs.existsSync(config.dev.outdir)) fs.mkdirSync(config.dev.outdir);
+    
+    postMethods.createPosts(posts);
+    // addHomePage(posts);
+  }
+
+  else {
+    // console.log('Sorry, I don\t know that command, please write simple-ssg1 --help for help');
+  }
+})
+
